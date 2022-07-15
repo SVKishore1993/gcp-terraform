074f376afc3ef9b9919b89ca6cbd2f0b7a43f845
Venkata Kishore <venkata.kishore@searce.com>
2022-07-15 15:46:18 +0530

    Updated project module

--- project/main.tf
+++ project/main.tf
@@ -14,14 +14,14 @@ locals {
 
 # Generate a random suffix to associate for project IDs to avoid conflicts
 resource "random_id" "this" {
-  for_each = toset(local.projects_environments)
+  for_each = toset(var.projects)
   byte_length = 2
 }
 
 resource "google_project" "my_project-in-a-folder" {
   for_each   = { for entry in local.projects_environments: "${entry.projects}.${entry.environments}" => entry }
   name       = "${each.value.environments}-${each.value.projects}-${var.parent_folder}"
-  project_id = replace(format("%s-%s", replace(lower(each.value.projects), " ", "-"), random_id.this[each.key].hex), "/-+/", "-")
+  project_id = replace(format("%s-%s", replace(lower(each.value.projects), " ", "-"), random_id.this[each.value.projects].hex), "/-+/", "-")
   folder_id  = split("/", var.folder_id)[1]
   billing_account  = var.billing_account_id
   auto_create_network = false
