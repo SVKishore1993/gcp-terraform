032643ae02a1829666784b443f3d23b92ea94f1e
Venkata Kishore <venkata.kishore@searce.com>
2022-07-15 14:53:35 +0530

    Updated project module

--- project/main.tf
+++ project/main.tf
@@ -14,7 +14,7 @@ locals {
 
 # Generate a random suffix to associate for project IDs to avoid conflicts
 resource "random_id" "this" {
-  for_each = toset(local.projects_environments)
+  for_each = toset(var.projects)
   byte_length = 2
 }
 
